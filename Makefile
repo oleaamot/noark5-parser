1617c8322f1eeb2ab9bf2b760e5203eba5bcef00
Ole Aamot <oka@oka.no>
2017-05-25 18:08:18 +0000

    Add -Wall -Wextra and target 'make check' (suggested by pere)

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 noark5-parser:	noark5-parser.c
-		cc noark5-parser.c -o noark5-parser `pkg-config --cflags --libs libxml-2.0 glib-2.0`
+		cc -Wall -Wextra noark5-parser.c -o noark5-parser `pkg-config --cflags --libs libxml-2.0 glib-2.0`
+
+check: runparser
+	./runparser
 
 clean:
 	rm -vf noark5-parser
--- noark5-parser.php
+++ noark5-parser.php
@@ -5,8 +5,9 @@ $dom = new DOMDOcument;
 while ($xml->read()) {
     $node = simplexml_import_dom($dom->importNode($xml->expand(), true));
     // now you can use $node without going insane about parsing
-    var_dump($node->dataset);    
-    // go to next <product />
+    print ($node->dataset->description . "\n");
+    var_dump($node->dataset);
+    // go to next <dataset>
     $xml->next('dataset');
 }
 
