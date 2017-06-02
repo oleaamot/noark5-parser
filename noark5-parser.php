278f44aa5c9c75fe4812e2796f5dad571f590aa2
Ole Aamot <oka@oka.no>
2017-06-02 11:03:54 +0000

    Server auth

--- noark5-parser.php
+++ noark5-parser.php
@@ -6,7 +6,23 @@ while ($xml->read()) {
     $node = simplexml_import_dom($dom->importNode($xml->expand(), true));
     // now you can use $node without going insane about parsing
     print ($node->dataset->description . "\n");
-    var_dump($node->dataset);
+    $data = array("username" => "admin", "password" => "password");
+    $data_string = json_encode($data);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "http://nikita.hioa.no:8092/noark5v4/auth");
+    curl_setopt($ch, CURLOPT_REFERER, 'https://nikita.hioa.no:8092/');
+    curl_setopt($ch, CURLOPT_USERAGENT, 'noark5-parser/0.1');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_string))
+    );
+    curl_exec($ch);
+    $page = curl_exec($ch);
+    var_dump($page);
+    // var_dump($node->dataset);
     // go to next <dataset>
     $xml->next('dataset');
 }
