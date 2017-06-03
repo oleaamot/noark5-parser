8b793343a23f824083a33a745cd0bc93d2bcb5b1
Ole Aamot <oka@oka.no>
2017-06-03 10:01:39 +0000

    Connect to server endpoint with token

--- noark5-parser.php
+++ noark5-parser.php
@@ -1,4 +1,21 @@
 <?php
+$data = array("username" => "admin", "password" => "password");
+$data_string = json_encode($data);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://nikita.hioa.no:8092/noark5v4/auth");
+curl_setopt($ch, CURLOPT_REFERER, 'https://nikita.hioa.no:8092/');
+curl_setopt($ch, CURLOPT_USERAGENT, 'noark5-parser/0.1');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($data_string))
+);
+curl_exec($ch);
+$page = curl_exec($ch);
+$data = json_decode($page);
+$token = $data->{"token"};
 $xml = new XMLReader();
 $xml->open("arkivuttrekk.xml");
 $dom = new DOMDOcument;
@@ -6,23 +23,23 @@ while ($xml->read()) {
     $node = simplexml_import_dom($dom->importNode($xml->expand(), true));
     // now you can use $node without going insane about parsing
     print ($node->dataset->description . "\n");
-    $data = array("username" => "admin", "password" => "password");
-    $data_string = json_encode($data);
+    // var_dump($node->dataset);
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "http://nikita.hioa.no:8092/noark5v4/auth");
+    curl_setopt($ch, CURLOPT_URL, "http://nikita.hioa.no:8092/noark5v4/");
     curl_setopt($ch, CURLOPT_REFERER, 'https://nikita.hioa.no:8092/');
     curl_setopt($ch, CURLOPT_USERAGENT, 'noark5-parser/0.1');
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $node->dataset->description);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Content-Type: application/json',
-        'Content-Length: ' . strlen($data_string))
+        'Accept: application/vnd.noark5-v4+json ',
+        'Authorization: ' . $token,
+        'Content-Type: application/vnd.noark5-v4+json',
+        'Content-Length: ' . strlen($node->dataset->description))
     );
     curl_exec($ch);
     $page = curl_exec($ch);
     var_dump($page);
-    // var_dump($node->dataset);
     // go to next <dataset>
     $xml->next('dataset');
 }
