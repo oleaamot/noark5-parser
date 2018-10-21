8dc31a2145e893354b0015e1285c25ac19e2a001
Ole Aamot <ole@gnome.org>
2018-10-21 12:23:20 +0200

    Update noark5_parser with OAuth2

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
 all:	check
 
+all:	run
+
+build:
+	mvn -Dmaven.test.skip=true clean install
+
+run:	build
+	mvn -f pom.xml spring-boot:run
+
+clean:
+	mvn -Dmaven.test.skip=true clean
+
 jar:
 	make -C com/
 
@@ -16,8 +27,8 @@ noark5-parser:	noark5-parser.c
 	cc noark5-parser.c -o noark5-parser `pkg-config --cflags --libs glib-2.0 libxml-2.0`
 
 noark5_parser:	noark5_parser.java
-	make -C com/
-	javac noark5_parser.java
+#	make -C com/
+#	javac -cp . noark5_parser.java
 
 clean:
 	rm -vf noark5-parser
--- noark5_parser.java
+++ noark5_parser.java
@@ -1,17 +1,65 @@
+package com.arkivarium;
 import com.arkivarium.Noark5Parser;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.hc.core5.*;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScheme;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
 
 class noark5_parser {
     public static void main (String[] args) {
 	Noark5Parser noark5_parser = new Noark5Parser();    
-	File selectedImportFile = new File("n5uttrekk-arkivstruktur.xml");
+	File selectedImportFile = new File("noark5_parser-arkivstruktur.xml");
 	File selectedExportFile = new File("noark5_parser-arkivstruktur.xml");
-	noark5_parser.parseImport(selectedImportFile);
-	noark5_parser.storeExport(selectedExportFile);
+	try {
+	    ArrayList<NameValuePair> postParameters;
+	    postParameters = new ArrayList<>();
+	    postParameters.add(new BasicNameValuePair("username", "admin"));
+	    postParameters.add(new BasicNameValuePair("password", "password"));
+	    postParameters.add(new BasicNameValuePair("grant_type", "password"));
+	    HttpHost targetHost = new HttpHost("localhost", 8092, "http");
+	    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+	    credsProvider.setCredentials(AuthScope.ANY,
+					 new UsernamePasswordCredentials(username, password));
+	    AuthCache authCache = new BasicAuthCache();
+	    authCache.put(targetHost, new BasicScheme());
+	    // Add AuthCache to the execution context
+	    final HttpClientContext context = HttpClientContext.create();
+	    context.setCredentialsProvider(credsProvider);
+	    context.setAuthCache(authCache);
+	    noark5_parser.parseImport(selectedImportFile);
+	    noark5_parser.storeExport(selectedExportFile);
+	} catch (MalformedURLException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
     }
 }
